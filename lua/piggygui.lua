297fb4566fb692429a5b0ab04d275a3c103b2cfb
Kingminer7 <100500023+Kingminer7@users.noreply.github.com>
2023-04-08 23:52:46 -0400

    Update piggygui.lua

--- lua/piggygui.lua
+++ lua/piggygui.lua
@@ -1,58 +1,8 @@
 local flyEnabled = false
+local flying = false
 
-function F_FLYSCRIPT()
-	local player = game.Players.LocalPlayer
-
-	local char = player.Character or player.CharacterAdded:Wait()
-	local hum = char:WaitForChild("Humanoid")
-
-	local cam = workspace.CurrentCamera
-	local uis = game:GetService("UserInputService")
-
-	local flightForce
-
-	local speed = 100
-
-	local flying = false
-	local function ToggleFlying()
-		if flyEnabled == true then
-			if not flying and hum.FloorMaterial == Enum.Material.Air then --Is double jumping - Start flying
-				flying = true
-
-				char.Animate.Disabled = true
-				for a,b in pairs(char:FindFirstChildWhichIsA("Humanoid").Animator:GetPlayingAnimationTracks()) do b:Stop() end
-				for a,b in pairs(char:FindFirstChildWhichIsA("Humanoid"):GetPlayingAnimationTracks()) do b:Stop() end
-
-				if flightForce ~= nil then flightForce:Destroy() end
-				flightForce = Instance.new("BodyVelocity", char.PrimaryPart)
-				flightForce.MaxForce = Vector3.new(math.huge,math.huge,math.huge)
-				flightForce.Velocity = hum.MoveDirection*speed
-
-				while flying do
-					hum:ChangeState("PlatformStanding") --This lets us see the move direction in 3 dimensions
-					hum:SetStateEnabled('GettingUp',false) --Prevents the character movement from glitching while flying
-
-					flightForce.Velocity = hum.MoveDirection*speed
-					wait()
-				end
-
-				--Stopped flying
-				flightForce:Destroy()
-				hum:SetStateEnabled('GettingUp',true)
-				char.Animate.Disabled = false
-			elseif flying then --Stop flying
-				flying = false
-			end
-		end
-	end
-
-	--//Input
-	uis.InputBegan:Connect(function(key, processed)
-		if key.KeyCode == Enum.KeyCode.Space then
-			ToggleFlying()
-		end
-	end)
-end
+local noclipEnabled = false
+local noclipping = false
 
 -- Gui to Lua
 -- Version: 3.2
@@ -66,11 +16,25 @@ local UICorner = Instance.new("UICorner")
 local UICorner_2 = Instance.new("UICorner")
 local corner = Instance.new("TextButton")
 local reopen = Instance.new("TextButton")
+local UIGridLayout = Instance.new("UIGridLayout")
+local fly = Instance.new("Frame")
+local flyLabel = Instance.new("TextLabel")
+local flyButton = Instance.new("TextButton")
+local UICorner_3 = Instance.new("UICorner")
+local noclip = Instance.new("Frame")
+local noclipLabel = Instance.new("TextLabel")
+local noclipButton = Instance.new("TextButton")
+local UICorner_4 = Instance.new("UICorner")
 
 --Properties:
 
-gui.ResetOnSpawn = false
-gui.Parent = game.Players.LocalPlayer.PlayerGui
+if(game["Run Service"]:IsStudio()) then
+	gui.Parent = game.Players.LocalPlayer.PlayerGui
+else
+	gui.Parent = game.CoreGui
+end
+
+gui.Name = "km7piggy"
 
 topbar.Name = "topbar"
 topbar.Parent = gui
@@ -115,6 +79,76 @@ reopen.TextColor3 = Color3.fromRGB(255, 255, 255)
 reopen.TextScaled = true
 reopen.TextSize = 14.000
 reopen.TextWrapped = true
+
+UIGridLayout.Parent = frame
+UIGridLayout.SortOrder = Enum.SortOrder.LayoutOrder
+UIGridLayout.CellSize = UDim2.new(0, 150, 0, 60)
+
+fly.Name = "fly"
+fly.Parent = frame
+fly.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+fly.BackgroundTransparency = 0.900
+fly.Size = UDim2.new(0, 100, 0, 100)
+
+flyLabel.Name = "Label"
+flyLabel.Parent = fly
+flyLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+flyLabel.BackgroundTransparency = 1.000
+flyLabel.Size = UDim2.new(1, -60, 0, 60)
+flyLabel.Font = Enum.Font.SourceSans
+flyLabel.Text = "Fly (Double Jump)"
+flyLabel.TextColor3 = Color3.fromRGB(197, 197, 197)
+flyLabel.TextScaled = true
+flyLabel.TextSize = 14.000
+flyLabel.TextWrapped = true
+
+flyButton.Name = "flyButton"
+flyButton.Parent = fly
+flyButton.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+flyButton.BackgroundTransparency = 0.600
+flyButton.Position = UDim2.new(1, -60, 0, 0)
+flyButton.Size = UDim2.new(0, 60, 0, 60)
+flyButton.Font = Enum.Font.SourceSans
+flyButton.Text = "Off"
+flyButton.TextColor3 = Color3.fromRGB(197, 197, 197)
+flyButton.TextSize = 40.000
+flyButton.TextWrapped = true
+
+UICorner_3.CornerRadius = UDim.new(1, 0)
+UICorner_3.Parent = flyButton
+
+noclip.Name = "noclip"
+noclip.Parent = frame
+noclip.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+noclip.BackgroundTransparency = 0.900
+noclip.Size = UDim2.new(0, 100, 0, 100)
+
+noclipLabel.Name = "Label"
+noclipLabel.Parent = noclip
+noclipLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+noclipLabel.BackgroundTransparency = 1.000
+noclipLabel.Size = UDim2.new(1, -60, 0, 60)
+noclipLabel.Font = Enum.Font.SourceSans
+noclipLabel.Text = "Noclip (RShift)"
+noclipLabel.TextColor3 = Color3.fromRGB(197, 197, 197)
+noclipLabel.TextScaled = true
+noclipLabel.TextSize = 14.000
+noclipLabel.TextWrapped = true
+
+noclipButton.Name = "noclipButton"
+noclipButton.Parent = noclip
+noclipButton.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+noclipButton.BackgroundTransparency = 0.600
+noclipButton.Position = UDim2.new(1, -60, 0, 0)
+noclipButton.Size = UDim2.new(0, 60, 0, 60)
+noclipButton.Font = Enum.Font.SourceSans
+noclipButton.Text = "Off"
+noclipButton.TextColor3 = Color3.fromRGB(197, 197, 197)
+noclipButton.TextSize = 40.000
+noclipButton.TextWrapped = true
+
+UICorner_4.CornerRadius = UDim.new(1, 0)
+UICorner_4.Parent = noclipButton
 
 -- Scripts:
 
@@ -174,8 +208,159 @@ local function DBGTEGR_fake_script() -- topbar.LocalScript
 end
 coroutine.wrap(DBGTEGR_fake_script)()
 
+function F_NOCLIPSCRIPT()
+	local player = game.Players.LocalPlayer
+
+	local char = player.Character or player.CharacterAdded:Wait()
+	local hum = char:WaitForChild("Humanoid")
+
+	local uis = game:GetService("UserInputService")
+	
+	
+	uis.InputBegan:Connect(function(inp,chat)
+		if chat then return end
+		if inp.KeyCode == Enum.KeyCode.RightShift then
+			if noclipEnabled then
+				noclip = not noclip
+				if noclip then
+					while noclip and noclipEnabled do
+						for _, v in pairs(char:GetDescendants()) do
+							if v:IsA("BasePart") then
+								v.CanCollide = false
+								if(not game["Run Service"]:IsStudio()) then game.Players.LocalPlayer.DevCameraOcclusionMode = Enum.DevCameraOcclusionMode.Invisicam end
+							end
+						end
+						wait()
+					end
+					for _, v in pairs(char:GetDescendants()) do
+						if v:IsA("BasePart") then
+							v.CanCollide = true
+							if(not game["Run Service"]:IsStudio()) then game.Players.LocalPlayer.DevCameraOcclusionMode = Enum.DevCameraOcclusionMode.Zoom end
+						end
+					end
+				end
+			end
+		end
+	end)
+end
+
+function F_FLYSCRIPT()
+	local player = game.Players.LocalPlayer
+
+	local char = player.Character or player.CharacterAdded:Wait()
+	local hum = char:WaitForChild("Humanoid")
+
+	local cam = workspace.CurrentCamera
+	local uis = game:GetService("UserInputService")
+
+	local flightForce
+	
+	local wPressed = false
+	local sPressed = false
+	local aPressed = false
+	local dPressed = false
+	
+	local lastJump = 0
+
+	local speed = 100
+	
+	local function ToggleFlying()
+		if flyEnabled == true then
+			if DateTime.now().UnixTimestampMillis - lastJump < 400 then
+				if not flying then --Is double jumping - Start flying
+					lastJump = 0
+					flying = true
+
+					char.Animate.Disabled = true
+					for a,b in pairs(char:FindFirstChildWhichIsA("Humanoid").Animator:GetPlayingAnimationTracks()) do b:Stop() end
+					for a,b in pairs(char:FindFirstChildWhichIsA("Humanoid"):GetPlayingAnimationTracks()) do b:Stop() end
+
+					if flightForce ~= nil then flightForce:Destroy() end
+					flightForce = Instance.new("BodyVelocity", char.PrimaryPart)
+					flightForce.MaxForce = Vector3.new(math.huge,math.huge,math.huge)
+					flightForce.Velocity = Vector3.new(0,0,0)
+
+					if (wPressed) then flightForce.Velocity += cam.CFrame.LookVector * speed end
+					if (sPressed) then flightForce.Velocity += cam.CFrame.LookVector * -speed end
+					if (aPressed) then flightForce.Velocity += cam.CFrame.RightVector * -speed end
+					if (dPressed) then flightForce.Velocity += cam.CFrame.RightVector * speed end
+
+					while flying do
+						hum:ChangeState("PlatformStanding") --This lets us see the move direction in 3 dimensions
+						hum:SetStateEnabled('GettingUp',false) --Prevents the character movement from glitching while flying
+						
+						flightForce.Velocity = Vector3.new(0,0,0)
+						
+						if (wPressed) then flightForce.Velocity += cam.CFrame.LookVector * speed end
+						if (sPressed) then flightForce.Velocity += cam.CFrame.LookVector * -speed end
+						if (aPressed) then flightForce.Velocity += cam.CFrame.RightVector * -speed end
+						if (dPressed) then flightForce.Velocity += cam.CFrame.RightVector * speed end
+						
+						wait()
+					end
+
+					--Stopped flying
+					flightForce:Destroy()
+					hum:SetStateEnabled('GettingUp',true)
+					char.Animate.Disabled = false
+				elseif flying then --Stop flying
+					flying = false
+				end
+			else
+				lastJump = DateTime.now().UnixTimestampMillis
+			end
+		end
+	end
+
+	--//Input
+	uis.InputBegan:Connect(function(key, chat)
+		if chat then return end
+		if key.KeyCode == Enum.KeyCode.Space then
+			ToggleFlying()
+		end
+		if key.KeyCode == Enum.KeyCode.W then
+			wPressed = true
+		elseif key.KeyCode == Enum.KeyCode.S then
+			sPressed = true
+		elseif key.KeyCode == Enum.KeyCode.A then
+			aPressed = true
+		elseif key.KeyCode == Enum.KeyCode.D then
+			dPressed = true
+		end
+	end)
+
+	uis.InputEnded:Connect(function(key)	
+		if key.KeyCode == Enum.KeyCode.W then
+			wPressed = false
+		elseif key.KeyCode == Enum.KeyCode.S then
+			sPressed = false
+		elseif key.KeyCode == Enum.KeyCode.A then
+			aPressed = false
+		elseif key.KeyCode == Enum.KeyCode.D then
+			dPressed = false
+		end
+	end)
+end
+
+flyButton.MouseButton1Click:Connect(function()
+	flyEnabled = not flyEnabled
+	if flyEnabled == true then flyButton.Text = "On" else flyButton.Text = "Off" end
+	if flying == true and flyEnabled == false then
+		flying = false
+	end
+end)
+
+noclipButton.MouseButton1Click:Connect(function()
+	noclipEnabled = not noclipEnabled
+	if noclipEnabled == true then noclipButton.Text = "On" else noclipButton.Text = "Off" end
+	if noclipping == true and noclipEnabled == false then
+		noclipping = false
+	end
+end)
+
 function startGui()
 	F_FLYSCRIPT()
+	F_NOCLIPSCRIPT()
 end
 
 startGui()
